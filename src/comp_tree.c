9931c3e2b1573f4f2e2618ec178638987db7e829
Lucas Kern <lucasgilbertokern@gmail.com>
2014-09-20 22:20:22 -0300

    Added implementation for the GraphViz visualization

--- src/comp_tree.c
+++ src/comp_tree.c
@@ -1,9 +1,14 @@
 #include "comp_tree.h"
+#include "gv.h"
+#include "iks_ast.h"
+#include "main.h"
+#include "symt.h"
 #include <stdlib.h>
 #include <stdio.h>
 
     nodeAST* createNodeAST(int type, nodeAST* next, void* symTable, ...)
     {
+		char* temp = (char*)calloc(32, sizeof(char));
         nodeAST* node = calloc(1, sizeof(nodeAST));
         node->type = type;
         node->next = next;
@@ -11,8 +16,45 @@
         node->c1 = NULL;
         node->c2 = NULL;
         node->c3 = NULL;
+		if(type == IKS_AST_IDENTIFICADOR || type == IKS_AST_FUNCAO)
+			gv_declare(type, (void*)node, (char*)(((DIC*)symTable)->token->description.string));
+		else if(type == IKS_AST_LITERAL)
+		{
+			if(((DIC*)symTable)->token->token == IKS_SIMBOLO_LITERAL_STRING)
+				gv_declare(type, (void*)node, (char*)(((DIC*)symTable)->token->description.string));
+			else if(((DIC*)symTable)->token->token == IKS_SIMBOLO_LITERAL_FLOAT)
+			{
+				sprintf(temp,"%f",(float)(((DIC*)symTable)->token->description.floating));
+				gv_declare(type, (void*)node, temp);
+			}
+			else if(((DIC*)symTable)->token->token == IKS_SIMBOLO_LITERAL_BOOL)
+			{
+				if((int)(((DIC*)symTable)->token->description.integer) == 0)
+					temp = "true";
+				else if((int)(((DIC*)symTable)->token->description.integer) == 1)
+					temp = "false";
+				gv_declare(type, (void*)node, temp);
+			}
+			else if(((DIC*)symTable)->token->token == IKS_SIMBOLO_LITERAL_INT)
+			{
+				sprintf(temp,"%d",(int)(((DIC*)symTable)->token->description.integer));
+				gv_declare(type, (void*)node, temp);
+			}
+			else if(((DIC*)symTable)->token->token == IKS_SIMBOLO_LITERAL_CHAR)
+			{
+				free(temp);
+				temp = (char*)calloc(2, sizeof(char));
+				temp[1] = '\0';
+				temp[0] = (char)(((DIC*)symTable)->token->description.character);
+				gv_declare(type, (void*)node, temp);
+			}
+		}
+		else
+			gv_declare(type, (void*)node, NULL);
         // va_list arg;
         // va_start (arg, next);//NodeAST type
+
+		gv_connect((void*)node,(void*)next);
     }
 
     void insertNodeASTChild(nodeAST* parent, nodeAST* child, int index)
@@ -25,6 +67,7 @@
                     trimNodeAST(parent->c1);
                 }
                 parent->c1 = child;
+				gv_connect((void*)parent,(void*)child);
                 break;
 
             case 2:
@@ -33,6 +76,7 @@
                     trimNodeAST(parent->c2);
                 }
                 parent->c2 = child;
+				gv_connect((void*)parent,(void*)child);
                 break;
 
             case 3:
@@ -41,6 +85,7 @@
                     trimNodeAST(parent->c3);
                 }
                 parent->c3 = child;
+				gv_connect((void*)parent,(void*)child);
                 break;
 
             default:
