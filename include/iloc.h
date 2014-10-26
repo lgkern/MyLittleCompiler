fbed7e2c43e8e8044c7904c9ce4e0331b2a6ed97
Lucas Kern <lucasgilbertokern@gmail.com>
2014-10-26 01:22:57 -0200

    Added base support for ILOC module

---
+++ include/iloc.h
@@ -0,0 +1,38 @@
+#ifndef _ILOC_H_
+#define _ILOC_H_
+
+#define IREGISTER 	1
+#define ICONSTANT 	2
+#define ILABEL		3
+
+typedef struct INST
+{
+	int instruction;
+	int argType[3];
+	int regs[3];
+	int consts[3];
+	char* labels[3];
+};
+
+typedef struct _ILIST ILIST;
+
+struct _ILIST{
+	INST*	instruction;
+    ILIST*	next;	
+};
+
+void 	printInstruction(INST* inst);
+
+/*
+ * The first 3 arguments define what type of arguments the instruction will have
+ * tArg1 being 1, means that the first left-oriented argument from the TAC will be a register
+ * tArg2 being 3, means that the second left-oriented argument from the TAC will be a label and so on...
+ */
+
+INST*	createInstruction(int tArg1, int tArg2, int tArg3, ...);
+void	destroyInstruction(INST* inst);
+
+void	mergeInstructionLists(ILIST* l1, ILIST* l2);
+void 	destroyInstructionList(ILIST* l);
+
+#endif
